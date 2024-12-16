f25b6cc2a70ae41ebd0e986ee6b87a468c58bc9d
Madis Pukkonen <mpukkonen@gmail.com>
2024-12-16 09:37:06 +0200

    Improve

--- config/.config/nvim/lua/plugins/telescope.lua
+++ config/.config/nvim/lua/plugins/telescope.lua
@@ -67,6 +67,11 @@ return {
             no_ignore = true,
           },
           buffers = { sort_mru = true, ignore_current_buffer = true },
+          live_grep = {
+            additional_args = function()
+              return { "--hidden" }
+            end,
+          },
         },
         extensions = {
           fzf = { fuzzy = true, override_generic_sorter = true, override_file_sorter = true, case_mode = "smart_case" },
